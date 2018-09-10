7f185068957d5952f3bd5703cb93229ecfe05458
freewind <nowindlee@gmail.com>
2018-09-10 20:57:22 +0800

    improve code

--- spec/test.js
+++ spec/test.js
@@ -2,9 +2,9 @@ import {expect} from 'chai'
 import sinon from 'sinon'
 import * as hello from '../hello'
 
-describe('sinon-chai', function () {
-    it('should call the mocked function', function () {
-        const stub = sinon.stub(hello, 'words').callsFake(function (name) {
+describe('sinon', function () {
+    it('should mock the given function', function () {
+        sinon.stub(hello, 'words').callsFake(function (name) {
             return `Hello, ${name} (changed)`
         })
         const value = hello.words('sinon')
